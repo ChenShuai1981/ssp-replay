09cb8ee56342bb8c3fa6253af42aa6695681eb35
ChenShuai <chenshuai@ChenShuaideMacBook-Air.local>
2016-02-24 17:08:22 +0800

    add converter in dedup

--- src/main/scala/com/vpon/ssp/report/dedup/flatten/exception/FlattenExceptions.scala
+++ src/main/scala/com/vpon/ssp/report/dedup/flatten/exception/FlattenExceptions.scala
@@ -4,13 +4,6 @@ class CouchbaseException(msg:String) extends RuntimeException(msg)
 class DelayedException(msg:String) extends RuntimeException(msg)
 class InvalidEventException(msg:String) extends RuntimeException(msg)
 
-class PlacementObjectNotFoundException(msg:String) extends RuntimeException(msg)
-class PublisherObjectNotFoundException(msg:String) extends RuntimeException(msg)
-
-class ExchangeRateObjectNotFoundException(msg:String) extends RuntimeException(msg)
-class PublisherSspTaxRateObjectNotFoundException(msg:String) extends RuntimeException(msg)
-class DspSspTaxRateObjectNotFoundException(msg:String) extends RuntimeException(msg)
-
 class SecretKeyNotFoundException(msg:String) extends RuntimeException(msg)
 class DecryptClearPriceException(msg:String) extends RuntimeException(msg)
 
