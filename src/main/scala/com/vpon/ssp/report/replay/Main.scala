1a7f22a3e7a36ec2356bc74a70049f569fccdea0
Casel Chen <casel.chen@vpon.com>
2016-03-14 10:01:08 +0800

    add decompressor

--- src/main/scala/com/vpon/ssp/report/replay/Main.scala
+++ src/main/scala/com/vpon/ssp/report/replay/Main.scala
@@ -3,6 +3,33 @@ package com.vpon.ssp.report.replay
 
 object Main extends App {
 
-  ReplayService.replay(201603110613L, 201603110613L)
+  println("Please input replay start time: [yyyyMMddHHmm]")
+  val startTimeStr = scala.io.StdIn.readLine
+
+  println()
+
+  println("Please input replay end time: [yyyyMMddHHmm]")
+  val endTimeStr = scala.io.StdIn.readLine
+
+  println()
+
+  val startTime = safeStringToLong(startTimeStr)
+  if (!startTime.isDefined) {
+    println(s"$startTimeStr is NOT a Long number!")
+  }
+  val endTime = safeStringToLong(endTimeStr)
+  if (!endTime.isDefined) {
+    println(s"$endTimeStr is NOT a Long number!")
+  }
+
+  println("Replay now ......")
+
+  ReplayService.replay(startTime.get, endTime.get)
+
+  def safeStringToLong(str: String): Option[Long] = try {
+    Some(str.toLong)
+  } catch {
+    case e:NumberFormatException => None
+  }
 
 }
